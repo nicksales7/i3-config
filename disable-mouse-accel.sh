2e83285d214c712d1cd8cba08a54ded24b480f32
nicksales7 <nicksales72@gmail.com>
2024-07-05 23:36:06 -0400

    Updated

---
+++ disable-mouse-accel.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+xinput set-prop "MX Anywhere 3S Mouse" "libinput Accel Profile Enabled" 0, 1
+xinput set-prop "Cooler Master Technology Inc. MM720 Gaming Mouse" "libinput Accel Profile Enabled" 0, 1
+
